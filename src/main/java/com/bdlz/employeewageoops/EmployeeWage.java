2c1f4cb33eddf69a3077e555e83653a47c5effcd
webPriya <priyachouhan1019@gmail.com>
2021-10-15 21:39:51 -0700

    Check employee is present or absent

--- src/main/java/com/bdlz/employeewageoops/EmployeeWage.java
+++ src/main/java/com/bdlz/employeewageoops/EmployeeWage.java
@@ -2,6 +2,13 @@ package com.bdlz.employeewageoops;
 
 public class EmployeeWage {
     public static void main(String[] args) {
-        System.out.println("WellCome to Employee Wage program");
+        System.out.println("WelCome to Employee Wage program");
+        int IS_FULL_TIME = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_FULL_TIME) {
+            System.out.println("employee is Present");
+        } else {
+            System.out.println("employee is Absent");
+        }
     }
 }
