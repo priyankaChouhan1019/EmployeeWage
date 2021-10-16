4a77a1b4648a4604cd78b267945119f04941d995
webPriya <priyachouhan1019@gmail.com>
2021-10-15 21:30:20 -0700

    added welcome message

---
+++ src/main/java/com/bdlz/employeewageoops/EmployeeWage.java
@@ -0,0 +1,7 @@
+package com.bdlz.employeewageoops;
+
+public class EmployeeWage {
+    public static void main(String[] args) {
+        System.out.println("WellCome to Employee Wage program");
+    }
+}
